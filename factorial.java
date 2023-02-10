3508567051cc4787a69670b5f16685a377e0b5e6
susanfg <susanafernandez@uma.es>
2023-02-10 20:13:55 +0100

    camio de long a BigInteger

--- factorial.java
+++ factorial.java
@@ -1,14 +1,16 @@
-import java.util.Scanner; public class factorial {
-public static long fact(long num) { if (num<=1) {
-return 1; } else {
-return num*fact(num-1); }
+import java.math.BigInteger; import java.util.Scanner;
+public class factorial {
+public static BigInteger fact(BigInteger num) { if (num.compareTo(BigInteger.ONE)<=0) {
+return BigInteger.ONE; } else {
+return num.multiply(fact(num.subtract(BigInteger.ONE))); }
 }
-return fac; }
 public static void main(String []args) {
-Scanner scanInput = new Scanner(System.in); System.out.print("Introduzca un numero: "); long num = scanInput.nextLong();
-if (num<0) {
-System.out.println("El factorial no esta definido"
-+ " para numeros negativos"); } else { long fac = fact(num);
+Scanner scanInput = new Scanner(System.in); System.out.print("Introduzca un numero: ");
+BigInteger num = BigInteger.valueOf(scanInput.nextLong());
+if (num.compareTo(BigInteger.ZERO)<0) { System.out.println("El factorial no esta definido"
+                         + " para numeros negativos");
+} else { BigInteger fac = fact(num);
 System.out.println("El factorial de "+num+" es "+fac); }
-      scanInput.close();
+             scanInput.close();
+      }
 }
